d2f32c9c959fe88d4383fa61ade79d4781f07446
David Conlan <david.conlan@csiro.au>
2021-11-06 15:52:10 +1000

    Add terminology using redcap fhir bridge, to allow authentication

--- src/script/terminology/REDCapFHIRTerminology.js
+++ src/script/terminology/REDCapFHIRTerminology.js
@@ -1,18 +1,40 @@
 import AbstractTerminology from 'pedigree/terminology/abstractTerminology';
 
-var FHIRTerminology = Class.create( AbstractTerminology, {
+/**
+ * This terminology is used by the redcap plugin to bridge from redcap to an external FHIR server.
+ * Both Lookup and Query web services use the same endpoint, but a type parameter is used to
+ * specify which is being carried out.
+ *
+ * The redcap webservice expects a POST, passing the following parameters:
+ *
+ * Lookup
+ * type -> 'lookup'
+ * system -> code system to be looked up
+ * code -> the id to lookup.
+ *
+ * Query
+ * type -> 'query'
+ * url -> valueset url
+ * filter -> search term
+ * count -> number of rows to return.
+ *
+ * The data returned will be the response from the fhir server.
+ *
+ * @type {klass}
+ */
+var REDCapFHIRTerminology = Class.create( AbstractTerminology, {
 
-    initialize: function($super, type, codeSystem, validIdRegex, searchCount, fhirBaseUrl, valueSet,
+    initialize: function($super, type, codeSystem, validIdRegex, searchCount, queryUrl, valueSet,
                          lookupAjaxOptions = {}, searchAjaxOptions = {}) {
         $super(type, codeSystem, validIdRegex, searchCount);
-        this._fhirBaseUrl = fhirBaseUrl;
+        this._queryUrl = queryUrl;
         this._valueSet     = valueSet;
         this._lookupAjaxOptions = lookupAjaxOptions;
         this._searchAjaxOptions = searchAjaxOptions;
     },
 
     getLookupURL: function(id){
-        return this._fhirBaseUrl + 'CodeSystem/$lookup?_format=json&system=' + this.getCodeSystem() + '&code=' + this.desanitizeID(id);
+        return this._queryUrl;
     },
 
     processLookupResponse: function(response){
@@ -29,7 +51,7 @@ var FHIRTerminology = Class.create( AbstractTerminology, {
     },
 
     getSearchURL: function(searchTerm){
-        return this._fhirBaseUrl + 'ValueSet/$expand?_format=json&url=' + this._valueSet + "&count=" + this.getSearchCount() + "&filter=" + searchTerm;
+        return this._queryUrl ;
     },
     processSearchResponse: function(response){
         if (response && response.responseText) {
@@ -46,12 +68,21 @@ var FHIRTerminology = Class.create( AbstractTerminology, {
         }
     },
     getLookupAjaxOptions: function(id){
-        return this._lookupAjaxOptions;
+        return { ...this._lookupAjaxOptions,
+            method: 'POST',
+            contentType: 'application/x-www-form-urlencoded',
+            parameters: {type: 'lookup', system: this.getCodeSystem(), code: this.desanitizeID(id)},
+        };
     },
     getSearchAjaxOptions: function(search){
-        return this._searchAjaxOptions;
+        return { ...this._searchAjaxOptions,
+            method: 'POST',
+            contentType: 'application/x-www-form-urlencoded',
+            parameters: { type: 'query', url: this._valueSet, filter: search, count: this.getSearchCount() },
+            processData: false
+        };
     },
 
 });
 
-export default FHIRTerminology;
+export default REDCapFHIRTerminology;
